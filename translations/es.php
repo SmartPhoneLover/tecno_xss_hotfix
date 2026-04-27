5ec21100051bb2641ce7195795158aece083c7dd
SmartPhoneLover <42898749+SmartPhoneLover@users.noreply.github.com>
2026-04-27 15:59:35 +0200

    Update es.php
    New translation

--- translations/es.php
+++ translations/es.php
@@ -8,6 +8,7 @@ $_MODULE['<{tecno_xss_hotfix}prestashop>tecno_xss_hotfix_d144f17c95431dddefef516
 $_MODULE['<{tecno_xss_hotfix}prestashop>tecno_xss_hotfix_3d43ac4696019b1f1061c573cc7ae5e4'] = 'Archivo no encontrado: %s';
 $_MODULE['<{tecno_xss_hotfix}prestashop>tecno_xss_hotfix_a5ca752abcdbc44f49aa95a30938afd0'] = 'Archivo sin permisos de escritura: %s';
 $_MODULE['<{tecno_xss_hotfix}prestashop>tecno_xss_hotfix_b433e8c5d1576d4de5c6281c99dd82d8'] = 'No se puede leer: %s';
+$_MODULE['<{tecno_xss_hotfix}prestashop>tecno_xss_hotfix_a3c3e87e1d3a9975740aa3fc77341aa1'] = 'Plantilla ya parcheada.';
 $_MODULE['<{tecno_xss_hotfix}prestashop>tecno_xss_hotfix_cc7c4469829e407aaa254fb553c7aee8'] = 'Plantilla: token vulnerable no encontrado (ya parcheado o versión PS diferente).';
 $_MODULE['<{tecno_xss_hotfix}prestashop>tecno_xss_hotfix_ceeceafe20074b78cb4ceb1a8899396e'] = 'No se puede crear la copia de seguridad: %s';
 $_MODULE['<{tecno_xss_hotfix}prestashop>tecno_xss_hotfix_b050994fb4c4254d3c1f613029281d12'] = 'No se puede escribir el archivo parcheado: %s';
